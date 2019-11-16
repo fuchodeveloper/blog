f5751bcd41f82ba2703e8d91a473ee4267576cf2
fuchodeveloper <fredrick.mgbeoma@andela.com>
2019-11-16 17:33:12 +0100

    GA;

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-71513306-3`,
       },
     },
     `gatsby-plugin-feed`,
